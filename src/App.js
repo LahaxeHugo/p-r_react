42cd8580e1d38d2597bedf15fe94f84c212a3399
Lahaxe Hugo <lahaxeh@gmail.com>
2021-06-16 16:46:13 +0200

    custom cursor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from 'react'
 
 function App() {
+  useEffect(() => {
+    document.addEventListener('mousemove', customCursorDelay);
+  }, []);
+
+  function customCursor(event) {
+    let cursor = document.getElementById('customcursor')
+    if(cursor == null) {
+        cursor = document.createElement('div')
+        cursor.id = 'customcursor'
+        document.body.append(cursor)
+    }
+
+    cursor.style.top = (event.clientY - cursor.offsetHeight / 2) + 'px'
+    cursor.style.left = (event.clientX - cursor.offsetWidth / 2) + 'px'
+  }
+
+  function customCursorDelay(event) {
+      setTimeout(function(){ 
+          customCursor(event)
+      }, 200);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
